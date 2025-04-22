18c82effd0710608e4026265109410c52968ea84
AnhHUY <hoanganhhuy200499@gmail.com>
2025-04-22 23:13:36 +0700

    save

--- src/util/ConnectedDatabase.java
+++ src/util/ConnectedDatabase.java
@@ -5,48 +5,33 @@
 
 package util;
 
-import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
-import com.microsoft.sqlserver.jdbc.SQLServerException;
 import java.sql.Connection; 
 import java.sql.DriverManager;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 public class ConnectedDatabase {
-private static SQLServerDataSource ds = new SQLServerDataSource();
     public static Connection getConnectedDB() {
-                 
-		var server = "localhost";  
-        var user = "sa";                 
-        var password = "161105";                        
-        var db = "T";              
-        var port = 1433;                  
-
-        ds.setUser(user);
-        ds.setPassword(password);
-        ds.setDatabaseName(db);
-        ds.setServerName(server);
-        ds.setPortNumber(port);
-        ds.setTrustServerCertificate(true);
-     
-    try {
-        return ds.getConnection();
-    } catch (SQLServerException ex) {
-       
-        Logger.getLogger(ConnectedDatabase.class.getName()).log(Level.SEVERE, null, ex);
+        Connection c = null;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://LAPTOP-2EA7CM4Q:1433;databaseName=QuanLyQuanAn;encrypt=false;characterEncoding=UTF-8";
+            String username = "sa";
+            String password = "123456789";
+            c = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return c;
     }
-    return null ;
-	}
     public static void closeConnectedDB(Connection c) {
-		try {
-			if(c!=null) {
-				c.close();
-				System.out.println("Đóng thành công !");
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("Không thể đóng ");
-		}
-	}
+        try {
+            if (c != null) {
+                c.close();
+                System.out.println("Đóng thành công !");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Không thể đóng ");
+        }
+    }
 
     public static com.sun.jdi.connect.spi.Connection getConnection() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
